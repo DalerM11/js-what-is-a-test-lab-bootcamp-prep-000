12e619e972b0f5daf0ac1495a012804bc4113729
Daler Muminov <muminovdaler@gmail.com>
2018-07-27 22:50:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,25 +5,3 @@ var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
 
-/*
-describe('what-is-a-test',() => {
-  describe('Name',() => {
-  it('returns "Susan"', () => {
-    expect(index.name).toEqual('Susan')
-  })
-})
-
-describe('Hieght', () => {
-  it('less than 40', () => {
-    expect(index.hieght).toBeLessThan(40)
-  })
-})
-
-describe('Message', () => {
-  it('gives the name and hieght', () => {
-    expect(index.name).toInclude(index.name)
-    expect(index.hieght).toInclude(index.hieght)
-  })
-})
-})
-*/
